d36547ab9972f5c162965c97f6a80ee8f1536f10
freezy <freezy@kodi.tv>
2019-06-22 22:38:35 +0200

    build: Add Bootstrap.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = () => {
@@ -12,9 +13,31 @@ module.exports = () => {
 				template: 'src/index.html',
 				minify: true,
 			}),
+			new CopyPlugin([
+				{ from: 'node_modules/dropzone/dist/min', to: 'assets/plugins/dropzone' },
+			]),
 		],
 		module: {
 			rules: [
+				{
+					test: /\.(scss|sass)$/,
+					use: [{
+						loader: 'style-loader', // inject CSS to page
+					}, {
+						loader: 'css-loader', // translates CSS into CommonJS modules
+					}, {
+						loader: 'postcss-loader', // Run postcss actions
+						options: {
+							plugins: function () { // postcss plugins, can be exported to postcss.config.js
+								return [
+									require('autoprefixer')
+								];
+							}
+						}
+					}, {
+						loader: 'sass-loader' // compiles Sass to CSS
+					}]
+				},
 			],
 		},
 		output: {
