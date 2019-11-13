08ef1078296d50f11f06b2f4faf7a1c7728b0539
freezy <freezy@kodi.tv>
2019-11-13 12:27:12 +0100

    build: Add workbox and set cache headers.

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,16 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = function() {
 	return webpackMerge(commonConfig({ devMode: false }), {
 		mode: 'production',
+		plugins: [
+			new WorkboxPlugin.GenerateSW({
+				swDest: 'sw.js',
+				clientsClaim: true,
+				skipWaiting: true,
+			}),
+		]
 	});
 };
