4e3d5bd15a5c93e3b7e14ab545c416e2f5f3fee6
michuNEEO <michael@neeo.com>
2019-11-27 00:57:34 +0100

    include more build information, rendering is missing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import './index.sass';
 
 import {FileCache} from './file-cache';
 import {Loader} from './loader';
+import * as timeago from 'timeago.js';
 
 const cache = new FileCache();
 const loader = new Loader(cache);
 
+const vpWebBuildTime = timeago.format(global.VPWEB_BUILD_TIMESTAMP);
+console.log('VPWEB BUILD was', vpWebBuildTime);
+
+
 window.vpw = { loader };
+window.build = {
+	vpWebBuildTime,
+	vpWebVersion: global.VPWEB_VERSION,
+	vpxJsVersion: global.VPXJS_VERSION,
+}
 
 cache.init()
 	.then(() => cache.get())
