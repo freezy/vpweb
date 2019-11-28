a5bb2c0338f72797ae78939bd05b18cfaedf2dbf
Michael Vogt <mvogt@control4.com>
2019-11-28 11:59:01 +0100

    more compact build information

--- src/index.js
+++ src/index.js
@@ -7,13 +7,10 @@ import * as timeago from 'timeago.js';
 const cache = new FileCache();
 const loader = new Loader(cache);
 
-const vpWebBuildTime = timeago.format(global.VPWEB_BUILD_TIMESTAMP);
-const vpxJsBuildTime = timeago.format(global.VPXJS_BUILD_TIMESTAMP);
-const detailText = 'VPX-JS build ' + vpxJsBuildTime +
-	'\nVPX-JS GIT HASH ' + global.VPXJS_GIT_HASH +
-	'\nVPX-JS GIT BRANCH ' + global.VPXJS_GIT_BRANCH +
-	'\nVPWEB v' + global.VPWEB_VERSION + ', built ' + vpWebBuildTime;
-
+const vpWebBuildTime = ' (' + timeago.format(global.VPWEB_BUILD_TIMESTAMP) + ')';
+const vpxJsBuildTime = ' (' + timeago.format(global.VPXJS_BUILD_TIMESTAMP) + ')';
+const detailText = 'VPX-JS ' + global.VPXJS_GIT_BRANCH + '@' + global.VPXJS_GIT_HASH + vpxJsBuildTime +
+	'\nVPWEB v' + global.VPWEB_VERSION + vpWebBuildTime;
 
 document.getElementById('vpx-version-details').textContent = 'VPX-JS v' + global.VPXJS_VERSION;
 document.getElementById('vpx-version-details').title = detailText;
