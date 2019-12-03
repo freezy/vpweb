5208ac52b589394e15b937fc78bbf14af1f0626d
freezy <freezy@kodi.tv>
2019-12-03 23:56:49 +0100

    home: Fix background.

--- src/renderer.js
+++ src/renderer.js
@@ -16,13 +16,13 @@ export class Renderer {
 		this._initControls();
 		this._initLights();
 		this._resetCamera();
-		document.getElementById('vpx-drop').classList.add('d-none');
+		document.getElementById('home').classList.add('d-none');
 		document.getElementById('close-playfield').classList.remove('d-none');
 	}
 
 	reset() {
 		cancelAnimationFrame(this.animationFrame);
-		document.getElementById('vpx-drop').classList.remove('d-none');
+		document.getElementById('home').classList.remove('d-none');
 		document.getElementById('close-playfield').classList.add('d-none');
 		document.body.removeChild(this.renderer.domElement);
 		document.body.removeChild(this.renderStats.dom);
